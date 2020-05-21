7fea04115be1fe429bb38ad51646326968ef5f9d
Akora Ing. DKB <debrahkwesibuabeng2@gmail.com>
2020-05-21 13:25:32 +0000

    Updated widget test

--- test/flutter_text_drawable_test.dart
+++ test/flutter_text_drawable_test.dart
@@ -11,12 +11,26 @@ void main() {
     await tester.pumpWidget(
       MaterialApp(
         home: Scaffold(
-          body: TextDrawable(text: text),
+          body: TextDrawable(
+            text: text,
+            isTappable: true,
+          ),
         ),
       ),
     );
 
-    // Expect to find the first character in the text supplied
+    // Expect to find the first character in the Text supplied but not an Icon
     expect(find.text(text[0]), findsOneWidget);
+    expect(find.byIcon(Icons.check), findsNothing);
+
+    // Tap on the TextDrawable widget
+    await tester.tap(find.byType(TextDrawable));
+
+    // Wait for widget to be rebuilt
+    await tester.pumpAndSettle();
+
+    // Expect to find an Icon instead of a Text
+    expect(find.byIcon(Icons.check), findsOneWidget);
+    expect(find.text(text[0]), findsNothing);
   });
 }
